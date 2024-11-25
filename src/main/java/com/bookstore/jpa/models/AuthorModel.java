c1b369bf8fc54a01a42c4294d3d679a2a41f60e1
Lucas <lucasgomesm1808@gmail.com>
2024-11-25 12:08:38 -0300

    @feat: implemented a bidirectional relationship and added the BookModel

--- src/main/java/com/bookstore/jpa/models/AuthorModel.java
+++ src/main/java/com/bookstore/jpa/models/AuthorModel.java
@@ -1,9 +1,12 @@
 package com.bookstore.jpa.models;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -17,6 +20,10 @@ public class AuthorModel implements Serializable {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToMany(mappedBy = "authors", fetch = FetchType.LAZY)
+    private Set<BookModel> books = new HashSet<>();
 
     public UUID getId() {
         return id;
