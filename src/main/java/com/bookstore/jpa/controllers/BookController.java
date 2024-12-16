e4351e28ec24babb4b436d75bc47ff67244466f3
Lucas <lucasgomesm1808@gmail.com>
2024-12-16 12:46:40 -0300

    created routes POST AND GET

---
+++ src/main/java/com/bookstore/jpa/controllers/BookController.java
@@ -0,0 +1,32 @@
+package com.bookstore.jpa.controllers;
+
+import com.bookstore.jpa.dtos.BookRecordDto;
+import com.bookstore.jpa.models.BookModel;
+import com.bookstore.jpa.services.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookstore/books")
+public class BookController {
+
+    public final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookModel>> getAllBooks() {
+        return ResponseEntity.status(HttpStatus.OK).body(bookService.getAllBooks());
+    }
+
+
+    @PostMapping
+    public ResponseEntity<BookModel> saveBook(@RequestBody BookRecordDto bookRecordDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.saveBook(bookRecordDto));
+    }
+}
